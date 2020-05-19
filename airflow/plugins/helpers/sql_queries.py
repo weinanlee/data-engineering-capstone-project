0819de69aeee1e2b2085b71f0e8c13137aecd638
Weinan Li <weianlee0322@gmail.com>
2020-05-19 14:37:25 +0200

    Delete staging immigration table and add new tables.

--- airflow/plugins/helpers/sql_queries.py
+++ airflow/plugins/helpers/sql_queries.py
@@ -2,40 +2,6 @@ class SqlQueries:
     table_name = ['staging_immigration','immigration', 'i94cit_i94res', 'i94mode','i94addr','i94visa','i94port']
 
 
-    staging_immigrant_table_create = """
-    CREATE TABLE IF NOT EXISTS public.staging_immigration (
-        Unnamed       INT,
-        cicid         FLOAT,
-        i94yr         FLOAT,
-        i94mon        FLOAT,
-        i94cit        FLOAT,
-        i94res        FLOAT,
-        i94port       VARCHAR,
-        arrdate       FLOAT,
-        i94mode       FLOAT,
-        i94addr       VARCHAR,
-        depdate       FLOAT,
-        i94bir        FLOAT,
-        i94visa       FLOAT,
-        count         FLOAT,
-        dtadfile      INT,
-        visapost      VARCHAR,
-        occup         VARCHAR,
-        entdepa       VARCHAR,
-        entdepd       VARCHAR,
-        entdepu       FLOAT,
-        matflag       VARCHAR,
-        biryear       FLOAT,
-        dtaddto       VARCHAR,
-        gender        VARCHAR,
-        insnum        FLOAT,
-        airline       VARCHAR,
-        admnum        FLOAT,
-        fltno         VARCHAR,
-        visatype      VARCHAR)
-    
-    """
-
     drop_table = """
     DROP TABLE IF EXISTS {};
     """
@@ -70,6 +36,56 @@ class SqlQueries:
         visatype VARCHAR
         );
     """
+
+    us_cities_demographics_table_create = """
+    CREATE TABLE IF NOT EXISTS public.us_cities_demographics (
+    city                       VARCHAR,
+    state                      VARCHAR, 
+    median_age                 FLOAT, 
+    male_population            FLOAT,
+    female_Population          FLOAT,
+    total_Population           FLOAT,
+    number_veterans            FLOAT,
+    foreign_born               FLOAT,
+    average_household_size     FLOAT,
+    state_code                 VARCHAR,
+    race                       VARCHAR,
+    count                      INT
+    );
+    """
+
+    us_cities_table_create = """
+    CREATE TABLE IF NOT EXISTS public.us_cities (
+    city                       VARCHAR,
+    state                      VARCHAR, 
+    median_age                 FLOAT, 
+    male_population            FLOAT,
+    female_Population          FLOAT,
+    total_Population           FLOAT,
+    number_veterans            FLOAT,
+    foreign_born               FLOAT,
+    average_household_size     FLOAT,
+    state_code                 VARCHAR
+    );
+    """
+    
+    airport_table_create = """
+    CREATE TABLE IF NOT EXISTS public.airport (
+    ident            VARCHAR,
+    type             VARCHAR,
+    name             VARCHAR,
+    elevation_ft     FLOAT,
+    continent        VARCHAR,
+    iso_country      VARCHAR,
+    iso_region       VARCHAR,
+    municipality     VARCHAR,
+    gps_code         VARCHAR,
+    iata_code        VARCHAR,
+    local_code       VARCHAR,
+    coordinates      VARCHAR
+    )
+    """
+
     ## I94CIT & I94RES - This format shows all the valid and invalid codes for processing 
     i94cit_i94res_table_create = """
     CREATE TABLE IF NOT EXISTS public.i94cit_i94res (
@@ -78,8 +94,7 @@ class SqlQueries:
         );
     """
     
- #    port_of_entry_codes_table_create = """
- #    """
+ 
 
     ## /* I94PORT - This format shows all the valid and invalid codes for processing */
     i94port_table_create = """
@@ -117,3 +132,46 @@ class SqlQueries:
         reason_for_travel VARCHAR
         );
     """
+    
+    us_state_race_table_create = """
+    CREATE TABLE IF NOT EXISTS public.us_state_race (
+    state_code VARCHAR,
+    state VARCHAR,
+    race VARCHAR,
+    race_ratio FLOAT
+    )
+    """
+
+    us_state_race_table_insert ="""
+    SELECT a.state_code,
+           a.state,
+           race,
+           cast (AVG(cast (a.count AS float)/b.total) AS decimal(2, 2))
+    FROM us_cities_demographics a
+    JOIN (
+    SELECT state_code,city,SUM(count) AS total
+    FROM us_cities_demographics
+    GROUP BY state_code,city
+    ORDER BY state_code,city) b
+    ON (a.state_code = b.state_code AND
+      a.city = b.city)
+    GROUP BY a.state_code,a.state,a.race
+    ORDER BY a.state_code
+    """
+
+    us_cities_table_insert = """
+    SELECT DISTINCT city,
+                    state, 
+                    median_age, 
+                    male_population,
+                    female_Population,
+                    total_Population,
+                    number_veterans,
+                    foreign_born,
+                    average_household_size,     
+                    state_code    
+    FROM public.us_cities_demographics                           
+    """ 
+    
+
+
